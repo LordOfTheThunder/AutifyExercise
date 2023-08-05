dc1671ba0c627b6936973123e4f49a6beccdd532
Max Kolchinsky <max.kolchinsky@gmail.com>
2023-08-05 19:08:03 +0300

    fix main

--- fetch_web_pages.rb
+++ fetch_web_pages.rb
@@ -1,14 +1,15 @@
 require_relative 'web_page_fetcher'
 
-if ARGV.empty?
-  puts "Usage: ruby fetch_web_pages.rb [--metadata] <URL1> <URL2> ..."
+urls = ARGV.select { |arg| !arg.start_with?('--') }
+
+if urls.empty?
+  puts "Usage: ruby fetch_web_pages.rb [--metadata] [--with_assets] <URL1> <URL2> ..."
 else
   base_dir = 'saved_web_pages'
   fetcher = WebPageFetcher.new(base_dir)
   metadata = ARGV.include?('--metadata')
   with_assets = ARGV.include?('--with_assets')
 
-  urls = ARGV.select { |arg| !arg.start_with?('--') }
   urls.each do |url|
     puts "Fetching #{url}..."
     fetcher.fetch_and_save(url, metadata: metadata, with_assets: with_assets)
